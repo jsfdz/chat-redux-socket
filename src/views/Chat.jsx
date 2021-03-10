3869af56b525564f7dd2e60b5c510002c5d60ba0
CrisanFdez <crisanfdez@gmail.com>
2021-03-10 18:54:56 -0500

    add endpoint

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -14,7 +14,7 @@ export const Chat = ({ location }) => {
     const [users, setUsers] = useState('')
     const [message, setMessage] = useState('')
     const [messages, setMessages] = useState([])
-    const ENDPOINT = 'https://react-chat-page.herokuapp.com/'
+    const ENDPOINT = 'https://academlo-chat.herokuapp.com/'
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search)
